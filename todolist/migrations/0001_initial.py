2d7098541bba99c2cdcbfcca47ed35d5668b363e
Pulkit2417 <66289429+Pulkit2417@users.noreply.github.com>
2023-12-15 07:42:09 +0530

    Update 0001_initial.py

--- todolist/migrations/0001_initial.py
+++ todolist/migrations/0001_initial.py
@@ -14,20 +14,42 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Tag',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                )),
                 ('name', models.CharField(max_length=50, unique=True)),
             ],
         ),
         migrations.CreateModel(
             name='ToDoItem',
             fields=[
-                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.BigAutoField(
+                    auto_created=True,
+                    primary_key=True,
+                    serialize=False,
+                    verbose_name='ID'
+                )),
                 ('timestamp', models.DateTimeField(auto_now_add=True)),
                 ('title', models.CharField(max_length=100)),
                 ('description', models.TextField(max_length=1000)),
                 ('due_date', models.DateField(blank=True, null=True)),
-                ('status', models.CharField(choices=[('OPEN', 'Open'), ('WORKING', 'Working'), ('DONE', 'Done'), ('OVERDUE', 'Overdue')], default='OPEN', max_length=10)),
-                ('tags', models.ManyToManyField(blank=True, to='todolist.tag')),
+                ('status', models.CharField(
+                    choices=[
+                        ('OPEN', 'Open'),
+                        ('WORKING', 'Working'),
+                        ('DONE', 'Done'),
+                        ('OVERDUE', 'Overdue')
+                    ],
+                    default='OPEN',
+                    max_length=10
+                )),
+                ('tags', models.ManyToManyField(
+                    blank=True,
+                    to='todolist.tag'
+                )),
             ],
         ),
     ]
