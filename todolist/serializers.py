ddec37cdf7deae4964071e80bd207555c8d3b66d
Pulkit2417 <66289429+Pulkit2417@users.noreply.github.com>
2023-12-15 07:38:47 +0530

    Update serializers.py

--- todolist/serializers.py
+++ todolist/serializers.py
@@ -1,19 +1,23 @@
 from rest_framework import serializers, viewsets
 from .models import ToDoItem, Tag
 
+
 class TagSerializer(serializers.ModelSerializer):
     class Meta:
         model = Tag
         fields = '__all__'
+
 
 class ToDoItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = ToDoItem
         fields = '__all__'
 
+
 class ToDoItemViewSet(viewsets.ModelViewSet):
     queryset = ToDoItem.objects.all()
     serializer_class = ToDoItemSerializer
+
 
 class TagViewSet(viewsets.ModelViewSet):
     queryset = Tag.objects.all()
