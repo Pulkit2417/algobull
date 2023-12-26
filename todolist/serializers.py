8be5ca6ba91809bf22e7e95353154c372b0b7cc6
Pulkit2417 <66289429+Pulkit2417@users.noreply.github.com>
2023-12-26 20:13:46 +0530

    Update serializers.py

--- todolist/serializers.py
+++ todolist/serializers.py
@@ -9,9 +9,25 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class ToDoItemSerializer(serializers.ModelSerializer):
+    tags = serializers.ListField(child=serializers.CharField(max_length=50), write_only=True)
+
     class Meta:
         model = ToDoItem
         fields = '__all__'
+
+    def create(self, validated_data):
+        tags_data = validated_data.pop('tags', [])  # Extract tags data
+
+        # Create or retrieve tags based on the input strings
+        tags = []
+        for tag_name in tags_data:
+            tag, _ = Tag.objects.get_or_create(name=tag_name)
+            tags.append(tag)
+
+        todo_item = ToDoItem.objects.create(**validated_data)
+        todo_item.tags.add(*tags)  # Add tags to the ToDoItem
+        return todo_item
+
 
 
 class ToDoItemViewSet(viewsets.ModelViewSet):
