483101b6bd10594d05c99807ec819d8c70bb6057
Pulkit2417 <66289429+Pulkit2417@users.noreply.github.com>
2023-12-26 20:13:23 +0530

    Update views.py

--- todolist/views.py
+++ todolist/views.py
@@ -2,11 +2,19 @@ from rest_framework import generics
 from .models import ToDoItem, Tag
 from .serializers import ToDoItemSerializer, TagSerializer
 from django.http import HttpResponse
+from rest_framework import status
+from rest_framework.response import Response
 
 
 class ToDoItemListCreateAPIView(generics.ListCreateAPIView):
     queryset = ToDoItem.objects.all()
     serializer_class = ToDoItemSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid():
+            self.perform_create(serializer)
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
 
 
 class ToDoItemDetailAPIView(generics.RetrieveUpdateDestroyAPIView):
